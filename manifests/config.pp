dd0aa1a4747f7b407a69bcbd46c4a463a75f7835
Javier Juarez <javier.juarez@gmail.com>
2017-01-17 12:22:50 +0100

    Bad template file name

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,7 @@ class jetty::config inherits jetty {
     owner   => $jetty::user,
     group   => $jetty::group,
     mode    => '0760',
-    content => template('jetty/jetty-defaults')
+    content => template('jetty/jetty-defaults.erb')
   }
 }
 
