456ccd0d802510a2fcab75e57797d73ad7597f68
Javier Juarez <javier.juarez@gmail.com>
2017-01-17 12:43:16 +0100

    An error in the template file name

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,7 @@ class jetty::config inherits jetty {
     owner   => $jetty::user,
     group   => $jetty::group,
     mode    => '0760',
-    content => epp('jetty/jetty-defaults.erb')
+    content => epp('jetty/jetty-defaults.epp')
   }
 }
 
