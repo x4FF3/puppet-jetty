84b265e0d01774b6e825be9b7ff8431e86beb37a
Javier Juarez <javier.juarez@gmail.com>
2017-01-19 18:50:31 +0100

    Support for a complete configuration of start.ini file

--- Rakefile
+++ Rakefile
@@ -1,14 +1,48 @@
-require 'bundler'
-Bundler.require(:rake)
-require 'rake/clean'
+require 'bundler/setup'
 require 'puppetlabs_spec_helper/rake_tasks'
-require 'puppet_blacksmith/rake_tasks'
+require 'puppet-lint/tasks/puppet-lint'
+require 'puppet-syntax/tasks/puppet-syntax'
+require 'metadata-json-lint/rake_task'
 
-CLEAN.include('spec/fixtures/', 'doc', 'pkg')
-CLOBBER.include('.tmp')
+begin
+  require 'puppet_blacksmith/rake_tasks'
+rescue LoadError => le# rubocop:disable Lint/HandleExceptions
+  $stderr.puts le.message
+end
 
-# Puppet list permissive with this kind of Taliban rules
-PuppetLint.configuration.send("disable_80chars")
+exclude_paths = [
+  "bundle/**/*",
+  "pkg/**/*",
+  "vendor/**/*",
+  "spec/**/*.pp",
+]
 
-task :default => [:clean, :spec, :lint]
+Rake::Task[:lint].clear
+PuppetLint.configuration.relative = true
+PuppetLint.configuration.fail_on_warnings = true
+PuppetLint.configuration.disable_80chars
+
+PuppetLint::RakeTask.new :lint do |config|
+  config.ignore_paths = exclude_paths
+end
+
+PuppetSyntax.exclude_paths = exclude_paths
+
+desc "Run acceptance tests"
+RSpec::Core::RakeTask.new(:acceptance) do |t|
+  t.pattern = 'spec/acceptance'
+end
+
+desc "Populate CONTRIBUTORS file"
+task :contributors do
+  system("git log --format='%aN' | sort -u > CONTRIBUTORS")
+end
+
+desc "Run syntax, lint, and spec tests."
+task :test => [
+  :metadata_lint,
+  :syntax,
+  :lint,
+  :spec,
+]
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -2,11 +2,16 @@
 # = class: jetty::install - The installation of jetty's stuff 
 class jetty::install inherits jetty {
 
-  $jetty_home            = "${jetty::root}/jetty"
-  $download_directory    = "jetty-distribution-${jetty::version}"
-  $download_file_name    = "${download_directory}.${jetty::archive_type}"
-  $download_url          = "${jetty::mirror}/org/eclipse/jetty/jetty-distribution/${jetty::version}/${download_file_name}"
-  $download_url_checksum = "${download_url}.sha1"
+  $_jetty_home             = "${jetty::root}/jetty"
+  $_jetty_tmp              = "${_jetty_home}/tmp"
+  $_jetty_logs             = "${_jetty_home}/log"
+  $_jetty_sh               = "${_jetty_home}/bin/jetty.sh"
+  $_jetty_base_webapps     = "${jetty::base}/webapps"
+  $_download_directory     = "jetty-distribution-${jetty::version}"
+  $_download_file_name     = "${_download_directory}.${jetty::archive_type}"
+  $_tmp_download_file_name = "/tmp/${_download_file_name}"
+  $_download_url           = "${jetty::mirror}/org/eclipse/jetty/jetty-distribution/${jetty::version}/${_download_file_name}"
+  $_download_url_checksum  = "${_download_url}.${jetty::checksum_type}"
 
   include '::archive'
 
@@ -14,7 +19,7 @@ class jetty::install inherits jetty {
     ensure_resource('user', $jetty::user, {
       shell  => '/bin/false',
       system => false,
-      home   => "${jetty_home}/tmp",
+      home   => $_jetty_tmp,
       gid    => $jetty::group,
     })
 
@@ -23,20 +28,20 @@ class jetty::install inherits jetty {
     })
   }
 
-  file { $jetty_home:
+  file { $_jetty_home:
     ensure => link,
-    target => "${jetty::root}/${download_directory}",
+    target => "${jetty::root}/${_download_directory}",
   } ->
-  file { "${jetty::root}/${download_directory}":
+  file { "${jetty::root}/${_download_directory}":
     ensure => directory,
     owner  => $jetty::user,
     group  => $jetty::group,
     mode   => '0754',
   } ->
-  archive { "/tmp/${download_file_name}":
+  archive { $_tmp_download_file_name:
     ensure          => present,
-    source          => $download_url,
-    checksum_url    => $download_url_checksum,
+    source          => $_download_url,
+    checksum_url    => $_download_url_checksum,
     checksum_verify => true,
     allow_insecure  => true,
     extract         => true,
@@ -50,13 +55,13 @@ class jetty::install inherits jetty {
 
   file { '/etc/init.d/jetty':
     ensure => link,
-    target => "${jetty_home}/bin/jetty.sh",
+    target => $_jetty_sh,
   } ->
   file { '/var/log/jetty':
     ensure => link,
-    target => "${jetty_home}/logs",
+    target => $_jetty_logs,
   } ->
-  file { "${jetty_home}/tmp":
+  file { $_jetty_tmp:
     ensure => directory,
     owner  => $jetty::user,
     group  => $jetty::group,
@@ -64,22 +69,17 @@ class jetty::install inherits jetty {
   }
 
   # Jetty Base setup
-  file { "${jetty::root}/web":
+  file { $jetty::base:
     ensure => directory,
     owner  => $jetty::user,
     group  => $jetty::group,
     mode   => '0754',
   } ->
-  file { "${jetty::root}/web/base":
-    ensure => directory,
-    owner  => $jetty::user,
-    group  => $jetty::group,
-    mode   => '0754',
-  } ->
-  file { "${jetty::root}/web/base/webapps":
+  file { $_jetty_base_webapps:
     ensure => directory,
     owner  => $jetty::user,
     group  => $jetty::group,
     mode   => '0754',
   }
 }
+
