b966c096a63990d6214fb4e04322541def6356d5
Javier Juarez <javier.juarez@gmail.com>
2017-01-19 01:34:09 +0100

    Enable the service

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,6 +5,7 @@ class jetty::service inherits jetty {
   service { 'Jetty Service':
     ensure     => $jetty::service_ensure,
     name       => 'jetty',
+    enable     => true,
     hasstatus  => true,
     hasrestart => true,
   }
