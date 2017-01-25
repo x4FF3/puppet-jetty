47fd089e279594961d3db5037e8bafdb853afe30
Javier Juarez <javier.juarez@gmail.com>
2017-01-25 23:58:31 +0100

    Groups the common attributes for the file resources

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,21 +2,19 @@
 # = class: jetty::config - This module manages the jetty Web Server configuration
 class jetty::config inherits jetty {
 
-  $_start_ini = "${jetty::base}/start.ini"
+  File {
+    owner => $jetty::user,
+    group => $jetty::group,
+    mode  => '0740',
+  }
 
   file { '/etc/default/jetty':
     ensure  => present,
-    owner   => $jetty::user,
-    group   => $jetty::group,
-    mode    => '0740',
     content => epp('jetty/jetty-defaults.epp'),
   }
 
-  file { $_start_ini:
+  file { "${::jetty::base}/start.ini":
     ensure  => present,
-    owner   => $jetty::user,
-    group   => $jetty::group,
-    mode    => '0740',
     content => epp('jetty/start.ini.epp'),
   }
 }
