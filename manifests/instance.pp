d2736f0dc6667412582bd82adb2713a64cefdb7d
Javier Juarez <javier.juarez@gmail.com>
2017-01-26 01:06:47 +0100

    Management of the logging configuration files

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,8 +1,8 @@
+##
+# 
 define jetty::instance(
   String $path = $title
   ) {
-
-  $_logging_module_configuration = "${path}/modules/logging.conf"
 
   File {
     owner => $::jetty::user,
@@ -13,15 +13,42 @@ define jetty::instance(
   file { [
     $path,
     "${path}/lib",
+    "${path}/lib/logging",
     "${path}/resources",
     "${path}/modules",
     "${path}/webapps"
     ]:
     ensure => directory,
   } ->
-  file { $_logging_module_configuration:
+  file { "${path}/modules/logging.conf":
     ensure => present,
     source => 'puppet:///modules/jetty/logging.mod',
+  } ->
+  file { "${path}/resources/jetty-logging.properties":
+    ensure => present,
+    source => 'puppet:///modules/jetty/jetty-logging.properties',
+  } ->
+  file { "${path}/resources/log4j.properties":
+    ensure  => present,
+    content => epp('jetty/log4j.properties.epp'),
+  }
+
+  archive { "${path}/lib/logging/slf4-api-1.6.6.jar":
+    ensure  => present,
+    extract => false,
+    source  => 'http://central.maven.org/maven2/org/slf4j/slf4j-api/1.6.6/slf4j-api-1.6.6.jar',
+  }
+
+  archive { "${path}/lib/logging/slf4-log4j2-1.6.6.jar":
+    ensure  => present,
+    extract => false,
+    source  => 'http://central.maven.org/maven2/org/slf4j/slf4j-log4j12/1.6.6/slf4j-log4j12-1.6.6.jar',
+  }
+
+  archive { "${path}/lib/logging/log4j-1.2.17.jar":
+    ensure  => present,
+    extract => false,
+    source  => 'http://central.maven.org/maven2/log4j/log4j/1.2.17/log4j-1.2.17.jar',
   }
 }
 
